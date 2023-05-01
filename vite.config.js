cda4fb57fefb1203a42dd0bc71c599371cf2381c
Aleksandr Mironenko <a.mironenko@initi.ru>
2023-05-01 17:27:36 +0300

    Changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [svelte()],
+    build: {
+        rollupOptions: {
+            output: {
+                dir: 'dist/checkUI',
+                entryFileNames: 'checkUI.js',
+                chunkFileNames: 'checkUI.js',
+                assetFileNames: 'checkUI.[ext]',
+            }
+        }
+    }
+
 })
